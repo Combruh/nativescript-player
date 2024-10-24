7613b1875c1e9af710c040341a38f5c95512b4e0
u4R2dK9s3u <ynl3@icloud.com>
2024-10-24 18:26:03 -0400

    first commit

---
+++ index.d.ts
@@ -0,0 +1,20 @@
+import { Observable } from '@nativescript/core';
+
+export interface StreamingOptions {
+    audioUrl?: string;
+    audioImageUrl?: string;
+    audioTitle?: string;
+    audioArtist?: string;
+}
+
+export declare class StreamingPlayer {
+    initPlayer(options: StreamingOptions): Promise<any>;
+    setMediaSession(options: StreamingOptions, play: Function, pause: Function, nextEpisode: Function, previousEpisode: Function): Promise<void>;
+    updatePlaybackState(isPlaying: boolean, currentTime: number): void;
+    seekTo(time: number, callback?: () => void): void;
+    play(): void;
+    pause(): void;
+    readonly currentTime: number;
+    readonly duration: number;
+    readonly isPlaying: number;
+}
